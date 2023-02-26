69e1bc88b41acb3c3963fac09907cfa84743af6b
faisalzamly <faisalzamly1@gmail.com>
2023-02-26 23:22:37 +0200

    change_password2

--- ecommerce_app/admin.py
+++ ecommerce_app/admin.py
@@ -7,16 +7,18 @@ from django.contrib.auth.models import User, Group
 
 
 
-
+class ProfileInline(admin.StackedInline):
+    model = Register_models
 class UserAdmin(admin.ModelAdmin):
     model = User
     fields = ["username","password","first_name","last_name"]
-
+    inlines = [ProfileInline]
 
 admin.site.unregister(User)
 
 admin.site.register(User,UserAdmin)
-admin.site.register(Register_models)
+# admin.site.register(Register_models)
+admin.site.register(Attribute)
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Attribute_Value)
--- ecommerce_app/urls.py
+++ ecommerce_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
         path("", index, name="index"),
         path("register/", register, name="register"),
         path("login/", login1, name="login"),
+        path("my_account/", my_account, name="my_account"),
 
 ]
